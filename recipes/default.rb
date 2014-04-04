f183983ee01b5b1c3385f5bbf2e06f3abd0de974
Marcy <marcy9114@gmail.com>
2014-04-04 10:41:59 +0900

    format device before make disk array

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,11 +8,23 @@
 #
 
 node[:raid][:devices].each do |dev|
-  if node['filesystem'].key?(dev) && node['filesystem'][dev].key?('mount') then
-    mount node['filesystem'][dev]['mount'] do
-      device dev
-      action :umount
-    end
+  mount node[:filesystem][dev][:mount] do
+    device dev
+    action :umount
+    only_if {
+      node[:filesystem].key?(dev) &&
+      node[:filesystem][dev].key?('mount') &&
+      node[:filesystem][dev][:mount] != node[:raid][:mount_point]
+    }
+  end
+  execute "format devides" do
+    command "fdformat #{dev}"
+    creates "/tmp/something"
+    action :run
+    only_if {
+      node[:filesystem].key?(dev) &&
+      node[:filesystem][dev][:fs_type] != node[:raid][:fs]
+    }
   end
 end
 
@@ -23,9 +35,9 @@ execute "create raid device" do
 end
 
 execute "make file system" do
-  command "mkfs.#{node[:raid][:fs]}"
+  command "mkfs.#{node[:raid][:fs]} #{node[:raid][:verbose]}"
   action :run
-  not_if "mkfs -V #{node[:raid][:verbose]} | grep -q #{node[:raid][:fs]}"
+  not_if {node[:filesystem][node[:raid][:verbose]][:fs_type] != node[:raid][:fs]}
 end
 
 directory node[:raid][:mount_point] do
