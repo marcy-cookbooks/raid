135037974b7bab79aa43d614cee5f0389b72d7d1
Marcy <marcy9114@gmail.com>
2014-12-08 14:17:33 +0900

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default['raid']['verbose']     = "/dev/md0"
 default['raid']['level']       = 0
-default['raid']['devices']     = ["/dev/sda", "/dev/sdb"]
+default['raid']['devices']     = ["/dev/sdb", "/dev/sdc"]
 default['raid']['fs']          = "xfs"
 default['raid']['mount_point'] = "/var"
